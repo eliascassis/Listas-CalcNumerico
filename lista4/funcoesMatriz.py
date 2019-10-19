0a5c4d54064ed3cd32f31e225b3112234bd539e2
caiosdeo <caiosdeo@hotmail.com>
2019-10-19 19:54:02 -0300

    Adicionando os criterios de convergencia

--- lista4/funcoesMatriz.py
+++ lista4/funcoesMatriz.py
@@ -218,34 +218,78 @@ def solveCholesky(G,b):
     Gt = G.T
     x = retrosubstituicao(Gt,y)
     return x
-                    
+
+# -----------------------------------------------------------------------------
+def criterioLinhas(A):
+
+    n = len(A)
+
+    alpha = np.zeros(n, dtype=float)
+
+    for i in range(n):
+        for j in range(1,n):
+
+            if(i != j):
+                alpha[i] += abs(A[i,j]/A[i,i]) 
+
+    if(max(alpha) < 1):
+        return True
+    else:
+        return False
+
+# -----------------------------------------------------------------------------
+def criterioSassenfeld(A):
+
+    n = len(A)
+
+    beta = np.zeros(n, dtype=float)
+
+    for i in range(n):
+        for j in range(1,i-1):
+            beta[i] += abs(A[i,j]) * beta[j]
+
+        for j in range(i+1,n):
+            beta[i] += abs(A[i,j]) 
+
+        beta[i] = beta[i]/abs(A[i,i])
+
+    if(max(beta) < 1):
+        return True
+    else:
+        return False
+
 # -----------------------------------------------------------------------------
 
 def jacobi(A, b, x0, epsilon=1e-4, kmax=100):
     
-    n = len(b)
-    x = np.copy(x0)
-    for k in range(kmax):
-        for i in range(n):
-            x[i] = (b[i] - np.delete(A[i], i) @ np.delete(x0, i)) / A[i,i] 
-        x0 = x
-        if np.linalg.norm(b - x0, 2) < epsilon:
-            return x0
-    print("Número máximo de iterações atingido!")
+    if(criterioLinhas(A)):
+        n = len(b)
+        x = np.copy(x0)
+        for k in range(kmax):
+            for i in range(n):
+                x[i] = (b[i] - np.delete(A[i], i) @ np.delete(x0, i)) / A[i,i] 
+            x0 = x
+            if np.linalg.norm(b - x0, 2) < epsilon:
+                return x0
+        print("Número máximo de iterações atingido!")
+
+    else:
+        print("Não passa do critério de linhas!")
 # -----------------------------------------------------------------------------
 
 def gaussSeidel(A, b, x0, epsilon, kmax):
     
-    n = len(b)
-    for k in range(kmax):
-        for i in range(n):
-            x0[i] = (b[i] - np.delete(A[i], i) @ np.delete(x0, i)) / A[i,i] 
-        if np.linalg.norm(b - x0, 2) < epsilon:
-            return x0
-    print("Número máximo de iterações atingido!")
+    if(criterioSassenfeld(A)):
+        n = len(b)
+        for k in range(kmax):
+            for i in range(n):
+                x0[i] = (b[i] - np.delete(A[i], i) @ np.delete(x0, i)) / A[i,i] 
+            if np.linalg.norm(b - x0, 2) < epsilon:
+                return x0
+        print("Número máximo de iterações atingido!")
 
-def column(matrix, i):
-    return [row[i] for row in matrix]
+    else:
+        print("Não passa do critério de Sassenfeld!")
 
 def inversa(A):
     """
